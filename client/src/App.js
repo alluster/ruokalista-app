de5b0e6f109d198aa7ddd69ac1aae11a5dee66bf
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-10-08 18:07:24 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,6 @@ function App() {
   return (
     <Router>
       <div>
-	
-       
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
 		<Route path="/item/:id" render={(props) => <Item {...props} />}/> 
           <Route path="/">
